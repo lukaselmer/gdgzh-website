1f9fac4a840cc4de825b6a257403ff336cf9c0a4
Lukas Elmer <lukas.elmer@gmail.com>
2017-11-23 19:51:59 +0100

    Comment unused / unimplemented code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -28,4 +28,10 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to gw!');
   }));
+  // it('should render title in a h1 tag', async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to gw!');
+  // }));
 });
