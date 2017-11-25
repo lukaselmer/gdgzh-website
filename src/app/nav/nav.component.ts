44bb483d605b4a4c01203d04203a540e7c5f1dad
Lukas Elmer <lukas.elmer@gmail.com>
2017-11-25 14:33:22 +0100

    Implement nav size depending on scroll position

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gw-nav',
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
-
-  constructor() { }
+  static MAX_HEIGHT = 47;
+  static MIN_HEIGHT = 30;
+  height = NavComponent.MAX_HEIGHT;
 
   ngOnInit() {
+    this.checkScroll();
   }
 
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const scrollPosition = window.pageYOffset;
+    const unrestrictedHeight = NavComponent.MAX_HEIGHT - scrollPosition;
+    this.height = this.clamp(unrestrictedHeight, NavComponent.MIN_HEIGHT, NavComponent.MAX_HEIGHT);
+  }
+
+  private clamp(value: number, minValue: number, maxValue: number) {
+    return Math.min(maxValue, Math.max(minValue, value));
+  }
 }
